8bf99d51172615bb1b7ab4d9564ec7f9494a40f4
Alerdy <alerdy@alerdy.co>
2016-12-24 14:31:13 -0600

    Removed custom font
    lpp-3ds is full of bugs, I can't stand it
    
    using a custom font caused issues

--- script/index.lua
+++ script/index.lua
@@ -1,7 +1,6 @@
 local white = Color.new(255,255,255)
 local green = Color.new(0,255,0)
 local url = "https://joshuadoes.com/projects/3DSHomebrew/BUILDS/nds-bootstrap/bootstrap-dldi.nds"
-font = Font.load("romfs:/font.ttf")
 
 function unicodify(str)
 	local new_str = ""
@@ -12,17 +11,16 @@ function unicodify(str)
 end
 
 function main()
-	Font.setPixelSizes(font,18)
 	Screen.refresh()
-	Font.print(font, 5,5, "nds-bootstrap updater", green, TOP_SCREEN)
-	Font.print(font, 5,30, "Press A to update nds-bootstrap", white, TOP_SCREEN)
-	Font.print(font, 5,50, "Press START to go to HOME menu", white, TOP_SCREEN)
-	Font.print(font, 5,70, "Press X to go to TWLauncher", white, TOP_SCREEN)
-	Font.print(font, 5,155, "Thanks to:", white, TOP_SCREEN)
-	Font.print(font, 5,170, "Alerdy for the updater", white, TOP_SCREEN)
-	Font.print(font, 5,185, "JoshuaDoes for hosting the builds", white, TOP_SCREEN)
-	Font.print(font, 5,200, "Rinnegatamante for lpp-3ds", white, TOP_SCREEN)
-	Font.print(font, 5,215, "ahezard for nds-bootstrap", white, TOP_SCREEN)
+	Screen.debugPrint(5,5, "nds-bootstrap updater", green, TOP_SCREEN)
+	Screen.debugPrint(5,30, "Press A to update nds-bootstrap", white, TOP_SCREEN)
+	Screen.debugPrint(5,50, "Press START to go to HOME menu", white, TOP_SCREEN)
+	Screen.debugPrint(5,70, "Press X to go to TWLauncher", white, TOP_SCREEN)
+	Screen.debugPrint(5,155, "Thanks to:", white, TOP_SCREEN)
+	Screen.debugPrint(5,170, "Alerdy for the updater", white, TOP_SCREEN)
+	Screen.debugPrint(5,185, "JoshuaDoes for hosting the builds", white, TOP_SCREEN)
+	Screen.debugPrint(5,200, "Rinnegatamante for lpp-3ds", white, TOP_SCREEN)
+	Screen.debugPrint(5,215, "ahezard for nds-bootstrap", white, TOP_SCREEN)
 	Screen.waitVblankStart()
 	Screen.flip()
 	while true do
@@ -41,14 +39,14 @@ function main()
 				Screen.flip()
 				Screen.clear(TOP_SCREEN)
 				if Network.isWifiEnabled() then
-					Font.print(font, 5,5, "Downloading the latest NDS file...", white, TOP_SCREEN)
+					Screen.debugPrint(5,5, "Downloading the latest NDS file...", white, TOP_SCREEN)
 					Network.downloadFile(url, "/bootstrap-dldi.nds")
-					Font.print(font, 5,20, "File downloaded!", white, TOP_SCREEN)
-					Font.print(font, 5,50, "Moving file to /_nds folder", white, TOP_SCREEN)
+					Screen.debugPrint(5,20, "File downloaded!", white, TOP_SCREEN)
+					Screen.debugPrint(5,50, "Moving file to /_nds folder", white, TOP_SCREEN)
 					System.renameFile("/bootstrap-dldi.nds","/_nds/bootstrap-dldi.nds")
-					Font.print(font, 5,95, "Done!", white, TOP_SCREEN)
-					Font.print(font, 5,110, "Press START to go to HOME menu", white, TOP_SCREEN)
-					Font.print(font, 5,125, "Press X to go to TWLauncher", white, TOP_SCREEN)
+					Screen.debugPrint(5,95, "Done!", white, TOP_SCREEN)
+					Screen.debugPrint(5,110, "Press START to go to HOME menu", white, TOP_SCREEN)
+					Screen.debugPrint(5,125, "Press X to go to TWLauncher", white, TOP_SCREEN)
 					
 					while true do
 						pad = Controls.read()
@@ -64,9 +62,9 @@ function main()
 						end
 					end
 				else
-					Font.print(font, 5,5, "WiFi is off! Please turn it on and retry!", white, TOP_SCREEN)
-					Font.print(font, 5,20, "Press START to go back to Home menu", white, TOP_SCREEN)
-					Font.print(font, 5,25, "Press X to go to TWLauncher", white, TOP_SCREEN)
+					Screen.debugPrint(5,5, "WiFi is off! Please turn it on and retry!", white, TOP_SCREEN)
+					Screen.debugPrint(5,20, "Press START to go back to Home menu", white, TOP_SCREEN)
+					Screen.debugPrint(5,25, "Press X to go to TWLauncher", white, TOP_SCREEN)
 					while true do
 						pad = Controls.read()
 						if pad ~= oldPad then
